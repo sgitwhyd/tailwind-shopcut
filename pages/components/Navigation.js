94da7089c86e317c811191d26088e6cc5c7fbf47
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2022-05-14 23:37:57 +0700

    edit navigation section

--- pages/components/Navigation.js
+++ pages/components/Navigation.js
@@ -3,7 +3,7 @@ import React from "react";
 const Navigation = () => {
   return (
     <>
-      <div className="sticky bottom-28 z-40 ml-[50%] w-3/4 -translate-x-1/2 rounded-full bg-white p-6 shadow-xl">
+      <div className="sticky bottom-4 z-40 ml-[50%] w-3/4 -translate-x-1/2 rounded-full bg-white p-6 shadow-xl">
         <div className="">center</div>
       </div>
     </>
