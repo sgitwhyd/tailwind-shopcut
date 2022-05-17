88266c66056aa5735ec7208b22c26915a08f39bb
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2022-05-17 20:24:43 +0700

    styling dot in nav bottom bar

--- pages/components/Navigation.js
+++ pages/components/Navigation.js
@@ -11,15 +11,15 @@ const Navigation = () => {
       <div className="sticky bottom-4 z-40 ml-[50%] w-4/6 -translate-x-1/2 rounded-full bg-white py-6  drop-shadow-xl">
         <div className="flex justify-between px-8">
           <div className="relative">
-            <div className="absolute -right-1 -top-1 h-3 w-3 rounded-full bg-red-600"></div>
+            <div className="absolute -right-1 -top-1 h-3 w-3 rounded-full bg-red-600 outline outline-2 outline-white"></div>
             <GrHomeRounded size={25} />
           </div>
           <div className="relative">
-            <div className="absolute -right-1 -top-1 h-3 w-3 rounded-full bg-red-600"></div>
+            <div className="absolute -right-1 -top-1 h-3 w-3 rounded-full bg-red-600 outline outline-2 outline-white"></div>
             <FiShoppingBag size={25} />
           </div>
           <div className="relative">
-            <div className="absolute -right-1 -top-1 h-3 w-3 rounded-full bg-red-600"></div>
+            <div className="absolute -right-1 -top-1 h-3 w-3 rounded-full bg-red-600 outline outline-2 outline-white"></div>
             <HiOutlineUser size={25} />
           </div>
         </div>
